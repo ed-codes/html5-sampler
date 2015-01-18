0b3d2397db1e71d7b4a2f2b1d7ae48013a719863
Eve Andersson <evean12@gmail.com>
2015-01-18 19:46:24 +1100

    Audio loader and controller updates

--- app/app.js
+++ app/app.js
@@ -16,3 +16,16 @@ var app = angular.module( 'html5Sampler', [
 ] ) ;
 
 
+
+
+var context ;
+window.addEventListener( 'load', init, false ) ;
+function init( ) {
+  try {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext ;
+    context = new AudioContext( ) ;
+  }
+  catch( e ) {
+    alert( 'Web Audio API is not supported in this browser' ) ;
+  }
+}
--- app/controllers.js
+++ app/controllers.js
@@ -1,20 +1,32 @@
 angular.module( 'html5Sampler.controllers', [ ] ).
 controller( 'tracksController', function( $scope ) {
-    $scope.tracksList = [
-      {
-        name: 'Track 1',
-        start: 20,
-        end: 40
-      },
-      {
-        name: 'Track 2',
-        start: 30,
-        end: 50
+    $scope.numTracks = 3 ;
+    $scope.tracksList = [ ] ;
+
+    // default track object
+
+    $scope.defaultTrack = {
+      duration: '120',
+      start: 20,
+      end: 100,
+      button: null,
+      audio: null
+    } ;
+
+    // scope methods
+
+    $scope.createTrack = function( index ) {
+      var obj = { name: 'Track ' + index } ;
+
+      for( var key in $scope.defaultTrack ) {
+        obj[ key ] = $scope.defaultTrack[ key ] ;
       }
-    ] ;
+
+      return obj ;
+    }
 
     $scope.add = function( ) {
-      $scope.newtrack = { name: 'Track ' + ( $scope.tracksList.length + 1 ) } ;
+      $scope.newtrack = $scope.createTrack( $scope.tracksList.length + 1 ) ;
       $scope.tracksList.push( $scope.newtrack ) ;
     }
 
@@ -22,4 +34,38 @@ controller( 'tracksController', function( $scope ) {
       var index = $scope.tracksList.indexOf( obj ) ;
       $scope.tracksList.splice( index, 1 ) ; 
     }
+
+    $scope.loadAudioFile = function( url ) {
+      var request = new XMLHttpRequest( ) ;
+
+      request.open( 'get', url, true ) ;
+      request.responseType = 'arraybuffer' ;
+
+      request.onload = function () {
+        context.decodeAudioData( request.response,
+          function( incomingBuffer ) {
+            $scope.playAudioFile( incomingBuffer ) ; 
+          }
+        ) ;
+      } ;
+
+      request.send( ) ;
+    }
+
+    $scope.playAudioFile = function( buffer ) {
+      var source = context.createBufferSource( ) ;
+
+      source.buffer = buffer ;
+      source.connect( context.destination ) ;
+      source.start( 0 ) ;
+    } ;
+
+
+    for( var i = 0 ; i < $scope.numTracks ; i++ ) {
+      var obj = $scope.createTrack( i + 1 ) ; { } ;
+      $scope.tracksList.push( obj ) ;
+    } ;
+
+    //$scope.loadAudioFile( 'test.wav' ) ;
+
 } ) ;
